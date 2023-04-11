525c76c0ad59412d26c3abf507ca1aa365eb370a
plimacarlos <plimacarlos@brq.com>
2023-04-11 16:38:56 -0300

    Initial Commit

---
+++ NavigationStore/NavigationStoreApp.swift
@@ -0,0 +1,17 @@
+////  NavigationStoreApp.swift
+//  NavigationStore
+//
+//  Created by Patricia Carlos on 11/04/23.
+//  
+//
+
+import SwiftUI
+
+@main
+struct NavigationStoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
